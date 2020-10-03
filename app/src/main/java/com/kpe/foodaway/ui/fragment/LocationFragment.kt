41ecfcb425544db869498ef7f7a7d7284058b2da
ma-za-kpe <makpalyy@gmail.com>
2020-10-03 23:15:41 +0300

    add location

---
+++ app/src/main/java/com/kpe/foodaway/ui/fragment/LocationFragment.kt
@@ -0,0 +1,38 @@
+package com.kpe.foodaway.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kpe.foodaway.R
+
+class LocationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment LocationFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            LocationFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
